3834a62571c425af88d71b62e1ffc2dd2bfa0828
naldurien <nicole.aldurien@gmail.com>
2021-03-09 11:53:03 -0500

    adding pics to nutritionFacts source file

--- nutritionApp.js
+++ nutritionApp.js
@@ -1,3 +1,4 @@
+
 let nutritionFactsUL = document.getElementById("nutritionFactsUL")
 
 
@@ -5,10 +6,9 @@ let nutritionFactsUL = document.getElementById("nutritionFactsUL")
 function displayNutrition() {
 for (let index= 0; index < nutritionFacts.length; index++) {
         let nutrition = nutritionFacts[index]
-        console.log(nutrition.fact)
         const nutritionItem = `
                                 <li id="nutritionItems">
-                                <img src="${nutrition.image}" width="200px" height="200px" /><br><br>
+                                <img src="${nutrition.image}" width="250px" height="250px" /><br><br>
                                 <b>Fact:</b>  <i>${nutrition.fact}</i><br><br>
                                 </li>
                                 `
