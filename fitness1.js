e03393fc1fb291e1356b8484833befb030b901c0
MIKEWARREN <mikeewarr@icloud.com>
2021-03-10 15:26:13 -0500

    nutrition changes

--- fitness1.js
+++ fitness1.js
@@ -6,14 +6,16 @@ const photoDIV = document.getElementById("photoDIV");
 function getNewPose() {
   let exercise = exercises[Math.floor(Math.random() * exercises.length)];
   const exerciseItem = ` <div>
+                        <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
                         <h1>  ${exercise.english_name} Pose</h1>
                         <h2>  Sanskrit: ${exercise.sanskrit_name}</h2>
                         <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" />
                         <p><b>Description:</b>  <br><i>${exercise.description}</i></p>
-                        <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
+                        
                         </div>
 `;
   photoDIV.innerHTML = exerciseItem;
 }
+
 
 getNewPose();
