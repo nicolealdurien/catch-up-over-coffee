82510c755247329e561dae8ed68358d12c84e5c7
SJ-CODES <summerjohnson@Summers-MacBook-Air.local>
2021-03-10 15:32:51 -0500

    updated info

--- fitness1.js
+++ fitness1.js
@@ -5,11 +5,11 @@ const photoDIV = document.getElementById("photoDIV");
 
 function getNewPose() {
   let exercise = exercises[Math.floor(Math.random() * exercises.length)];
-  const exerciseItem = ` <div>
+  const exerciseItem = ` <div id="fitnessDisplay">
                         <h1>  ${exercise.english_name} Pose</h1>
                         <h2>  Sanskrit: ${exercise.sanskrit_name}</h2>
                         <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" />
-                        <p><b>Description:</b>  <br><i>${exercise.description}</i></p>
+                        <p><b>How to achieve pose:</b>  <br><b>${exercise.description}<b></p>
                         <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
                         </div>
 `;
@@ -17,3 +17,5 @@ function getNewPose() {
 }
 
 getNewPose();
+
+
