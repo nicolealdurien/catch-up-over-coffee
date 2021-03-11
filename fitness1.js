e553f48a8efdea1510ff97d7f3259763da6357d0
SJ-CODES <summerjohnson@Summers-Air.attlocal.net>
2021-03-10 23:34:43 -0500

    minor changes to css

--- fitness1.js
+++ fitness1.js
@@ -10,7 +10,7 @@ function getNewPose() {
                         <h1>  ${exercise.english_name} Pose</h1>
                         <h2>  Sanskrit: ${exercise.sanskrit_name}</h2>
                         <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" />
-                        <p><b>How to achieve pose:</b>  <br><b>${exercise.description}<b></p>
+                        <h3><b>How to achieve pose:</b></h3>  <p><b>${exercise.description}<b></p>
                         <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
 
                         </div>
