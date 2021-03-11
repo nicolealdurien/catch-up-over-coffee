ed7fd51d7d84958e79b1e17c67ad119cbbdcd465
naldurien <nicole.aldurien@gmail.com>
2021-03-10 20:47:52 -0500

    added hover to 'get new pose' button

--- fitness1.js
+++ fitness1.js
@@ -6,19 +6,17 @@ const photoDIV = document.getElementById("photoDIV");
 function getNewPose() {
   let exercise = exercises[Math.floor(Math.random() * exercises.length)];
 
-  const exerciseItem = ` <div id="fitnessDisplay">
+  const exerciseItem = ` 
+                        <div id="fitnessDisplay">
                         <h1>  ${exercise.english_name} Pose</h1>
                         <h2>  Sanskrit: ${exercise.sanskrit_name}</h2>
                         <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" />
                         <p><b>How to achieve pose:</b>  <br><b>${exercise.description}<b></p>
-                        <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
-
+                        <a href="#btnNewPose" onclick="getNewPose()" id ="btnNewPose" class="button">Get New Pose</a>
                         </div>
-`;
+                        `;
   photoDIV.innerHTML = exerciseItem;
 }
 
 
 getNewPose();
-
-
