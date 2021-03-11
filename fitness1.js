0aef66a47ba7d5ad479fb7c601b41185b000f430
naldurien <nicole.aldurien@gmail.com>
2021-03-11 10:47:05 -0500

    quick fitness changes

--- fitness1.js
+++ fitness1.js
@@ -10,7 +10,7 @@ function getNewPose() {
                         <div id="fitnessDisplay">
                         <h1>  ${exercise.english_name} Pose</h1>
                         <h2>  Sanskrit: ${exercise.sanskrit_name}</h2>
-                        <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" />
+                        <img src="${exercise.img_url}" width="150px" height="150px" class="exerciseImage" /><br><br>
                         <button onclick=getNewPose() class="newPoseBtn">Get New Pose</button>
                         <h3><b>How to achieve pose:</b></h3>  <p><b>${exercise.description}<b></p>
                         
